89de2921167a8dd5cd3fe51426dd20058f69ed8f
escmanager <34496116+escmanager@users.noreply.github.com>
2018-01-22 22:34:12 +0900

    gesc

--- modules/menuItems.js
+++ modules/menuItems.js
@@ -452,16 +452,16 @@ let menuTempl = function (webviews) {
         const nodeSubmenu = [];
 
         const ethClient = ClientBinaryManager.getClient('eth');
-        const gethClient = ClientBinaryManager.getClient('geth');
+        const gethClient = ClientBinaryManager.getClient('gesc');
 
         if (gethClient) {
             nodeSubmenu.push({
-                label: `Geth ${gethClient.version}`,
+                label: `Gesc ${gethClient.version}`,
                 checked: ethereumNode.isOwnNode && ethereumNode.isGeth,
                 enabled: ethereumNode.isOwnNode,
                 type: 'checkbox',
                 click() {
-                    restartNode('geth', null, 'fast', webviews);
+                    restartNode('gesc', null, 'fast', webviews);
                 },
             });
         }
